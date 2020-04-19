59c2215621fc283ba202cd05b6ea68057faa642f
TheShaltys <44344165+TheShaltys@users.noreply.github.com>
2020-04-19 17:17:18 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import torch
 import architecture as arch
-from datetime import datetime
+from datetime import datetime, date
 
 model_path = sys.argv[1]  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
 output_file = sys.argv[2]
@@ -23,6 +23,10 @@ for k, v in model.named_parameters():
 model = model.to(device)
 
 print('Model path {:s}. \nTesting...'.format(model_path))
+print('Frames: ', len(glob.glob(test_img_folder)))
+
+dt_start = datetime.strptime('07:50:51', '%H:%M:%S').time()
+start_date = date.today()
 
 idx = 0
 for path in glob.glob(test_img_folder):
@@ -43,3 +47,13 @@ for path in glob.glob(test_img_folder):
     output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
     output = (output * 255.0).round()
     cv2.imwrite(output_file.format(base), output)
+ 
+dt_end = datetime.now().time()
+end_date = date.today()
+
+combination_start = datetime.combine(start_date, dt_start)
+combination_end = datetime.combine(end_date, dt_end)
+
+duration = datetime.strptime(str(combination_end - combination_start), '%H:%M:%S.%f')
+
+print('Duration: ',duration.strftime('%H:%M:%S'), ', Frames: ', len(glob.glob(test_img_folder)))
